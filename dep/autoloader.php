4b37d9993367db7f3645751d1c30a8ee1fe0fcc6
Jeremy Seago <seagoj@gmail.com>
2012-12-14 16:11:57 -0600

    Adjust autoloader.php path to lib/Autoload.php

--- dep/autoloader.php
+++ dep/autoloader.php
@@ -1,4 +1,17 @@
 <?php
-require __DIR__.'/lib/Devtools/Autoload.php';
+/**
+ * Autoloader file
+ *
+ * @name      autoloader.php
+ * @category  Devtools
+ * @package   Seagoj
+ * @author    Jeremy Seago <seagoj@gmail.com>
+ * @copyright 2012 Jeremy Seago
+ * @license   http://opensource.org/licenses/mit-license.php, MIT
+ * @version   1.0
+ * @link      https://github.com/seagoj/devtools
+ */
+
+require '../lib/Devtools/Autoload.php';
 
 Devtools\Autoload::register();
--- src/AutoloadTest.php
+++ src/AutoloadTest.php
@@ -9,13 +9,13 @@
  * @copyright 2012 Jeremy Seago
  * @license   http://opensource.org/licenses/mit-license.php, MIT
  * @version   1.0
- * @link      https://github.com/seagoj/lib.autoload
+ * @link      https://github.com/seagoj/devtools
  */
 
 namespace NewProject;
 
 print "<div>BoF</div>";
-require_once $_SERVER['DOCUMENT_ROOT'].'/autoload.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/autoloader.php';
 
 $dbg = new \Devtools\Dbg();
 $redis = new \Predis\Client();
