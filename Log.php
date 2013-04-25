9f46fd6f7ef8d6f6c2a62d3495fdaadba617df06
Jeremy Seago <seagoj@gmail.com>
2013-04-25 18:48:22 -0500

    Adds parameter to specify log file in the constructor

--- Log.php
+++ Log.php
@@ -1,41 +1,45 @@
-    <?php
-    class Log {
-        private $testCount;
+<?php
+    
+namespace Devtools;
+    
+class Log {
+    private $testCount;
 
-        public function __construct()
-        {
-            $this->logToFile(date("m-d-Y H:i:s"));
-            $this->testCount = 0;
-        }
-
-        private function logToFile($content, $result='EMPTY', $file="hook.log")
-        {
-            $endline = "\r\n";
-            $content = $this->tapify($content, $result);
-
-            print "<div>$content</div>";
-            return file_put_contents($file, $content.$endline, FILE_APPEND);
-        }
-
-        private function tapify($content, $result) {
-            $nextTest = $this->testCount+1;
-            $prefix = 'ok '.$nextTest.' - ';
-            
-            if($result!=='EMPTY') {
-                    $this->testCount = $nextTest;
-                    $content = $prefix.$content;
-                if(!$result) {
-                    $content = 'not '.$content;
-                }
-            }
-
-            return $content;
-        }
-        
-        public function __destruct()
-        {
-            $postfix = '1..'.$this->testCount;
-            $this->logToFile($postfix);
-            $this->logToFile("\r\n");
-        }
+    public function __construct($file='Log.log')
+    {
+        $this->file = $file;
+        $this->logToFile(date("m-d-Y H:i:s"));
+        $this->testCount = 0;
     }
+
+    private function logToFile($content, $result='EMPTY')
+    {
+        $endline = "\r\n";
+        $content = $this->tapify($content, $result);
+
+        print "<div>$content</div>";
+        return file_put_contents($this->file, $content.$endline, FILE_APPEND);
+    }
+
+    private function tapify($content, $result) {
+        $nextTest = $this->testCount+1;
+        $prefix = 'ok '.$nextTest.' - ';
+            
+        if($result!=='EMPTY') {
+                $this->testCount = $nextTest;
+                $content = $prefix.$content;
+            if(!$result) {
+                $content = 'not '.$content;
+            }
+        }
+
+        return $content;
+    }
+        
+    public function __destruct()
+    {
+        $postfix = '1..'.$this->testCount;
+        $this->logToFile($postfix);
+        $this->logToFile("\r\n");
+    }
+}
