00dca84b51c9be062ea08fade924b9a0fce17c81
Jeremy Seago <seagoj@gmail.com>
2013-04-25 16:21:37 -0500

    Remove unneeded calls from authTest

--- tests/authTest.php
+++ tests/authTest.php
@@ -4,13 +4,10 @@ class HookTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $_REQUEST['payload'] = file_get_contents('tests/payload.json');
-        $_SERVER['HTTP_CLIENT_IP'] = '127.0.0.1';
     }
 
     public function tearDown()
     {
-        unset($_REQUEST);
     }
 
     public function authTest()
