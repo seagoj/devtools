6d638a7be54511a5e3e4df74dfc72d5eb34f940a
Jeremy Seago <seagoj@gmail.com>
2013-04-25 16:28:17 -0500

    Sets hard path to Log from authTest.php

--- Log.php
+++ Log.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
     class Log {
         private $testCount;
 
--- tests/authTest.php
+++ tests/authTest.php
@@ -2,8 +2,12 @@
 
 class HookTest extends PHPUnit_Framework_TestCase
 {
+    private $log;
+
     public function setUp()
     {
+        require "Log.php";
+        $this->log = new Log();
     }
 
     public function tearDown()
@@ -14,6 +18,7 @@ class HookTest extends PHPUnit_Framework_TestCase
     {
         $auth = new \Devtools\Auth("user", "password");
         $this->assertInstanceOf('Auth', $auth);
+        $this->log->logToFile('$auth is instance of Auth','EMPTY','authTest.log');
     }
 
     public function validateTest()
