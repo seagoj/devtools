0e6834f72844f5c951a6dc639c5710f696a81d09
Jeremy Seago <seagoj@gmail.com>
2013-04-30 16:44:31 -0500

    Switches to use simple bootstrap.php

--- tests/AutoloadTest.php
+++ tests/AutoloadTest.php
@@ -4,6 +4,7 @@ class AutoloadTest extends PHPUnit_Framework_TestCase
 {
     public function testAutoload()
     {
+        require_once 'autoloader.php';
         $this->assertTrue(false);
     }
 }
