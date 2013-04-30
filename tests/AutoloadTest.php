c410a6a887b215e3ca7126f9510a0a5050faa2df
Jeremy Seago <seagoj@gmail.com>
2013-04-30 15:41:26 -0500

    Strips down AutoloadTest

--- tests/AutoloadTest.php
+++ tests/AutoloadTest.php
@@ -2,18 +2,8 @@
 
 class AutoloadTest extends PHPUnit_Framework_TestCase
 {
-    public function setup()
-    {
-    }
-
-    public function tearDown()
-    {
-    }
-
     public function constructTest()
     {
         $this->assertTrue(true);
-//        $log = new \Devtools\Log();
-//        $this->assertInstanceOf('Log', $log);
     }
 }
