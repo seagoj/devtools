bfcb0201efd440f0bce142ce70409707a4303fd9
Jeremy Seago <seagoj@gmail.com>
2013-04-30 17:32:56 -0500

    Dumps __autoload function

--- Autoload.php
+++ Autoload.php
@@ -60,7 +60,10 @@ class Autoload
     {
 //        echo "call ".__METHOD__;
 //        spl_autoload_register(array(new self, '_autoload'), true, $prepend);
-//          spl_autoload_register(array(new self, '_autoload'));
+
+        spl_autoload_register(array(new self, '_autoload'));
+        var_dump(spl_autoload_functions);
+
 
     }
 
