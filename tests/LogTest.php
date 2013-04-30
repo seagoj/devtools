95a89128fb2476e7daf227a3b468b2bebf65e877
Jeremy Seago <seagoj@gmail.com>
2013-04-30 14:46:17 -0500

    Drops explicit call to Log.php and relies on tests/autoloader.php

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -4,17 +4,14 @@ class LogTest extends PHPUnit_Framework_TestCase
 {
     public function setup()
     {
-        
     }
 
     public function tearDown()
     {
-        
     }
 
-    public function logTest()
+    public function LogTest()
     {
-        require "Log.php";
         $log = new \Devtools\Log();
         $this->assertInstanceOf('Log', $log);
     }
@@ -28,6 +25,16 @@ class LogTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue(is_file($options['file']));
         $this->assertTrue(file_get_contents($options)!=='');
+    }
+
+    public function _configTest()
+    {
+        
+    }
+
+    public function _tapifyTest()
+    {
+            
     }
 }
 
