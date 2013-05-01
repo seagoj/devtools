768bddcc0356870a7aeb8543fe00a966a795446c
Jeremy Seago <seagoj@gmail.com>
2013-04-30 20:33:47 -0500

    Adds instanceOf test

--- tests/AutoloadTest.php
+++ tests/AutoloadTest.php
@@ -4,6 +4,8 @@ class AutoloadTest extends PHPUnit_Framework_TestCase
 {
     public function testAutoload()
     {
+        $log = new \Devtools\Log();
+        $this->InstanceOf("\Devtools\Log", $log);
         $this->assertTrue(true);
     }
 }
