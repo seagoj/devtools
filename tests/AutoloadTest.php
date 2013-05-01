620e04433a23f0041896d96b83c611c3bf242a82
Jeremy Seago <seagoj@gmail.com>
2013-05-01 12:30:22 -0500

    Corrects call to assertInstanceOf

--- tests/AutoloadTest.php
+++ tests/AutoloadTest.php
@@ -5,7 +5,7 @@ class AutoloadTest extends PHPUnit_Framework_TestCase
     public function testAutoload()
     {
         $log = new \Devtools\Log();
-        $this->InstanceOf("\Devtools\Log", $log);
+        $this->assertInstanceOf("\Devtools\Log", $log);
         $this->assertTrue(true);
     }
 }
