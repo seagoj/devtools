f785bbd0aa48c3ec214ef026f6547bcfa2914cbe
Jeremy Seago <seagoj@gmail.com>
2013-05-14 13:58:49 -0500

    Uncomments all but validation test

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -6,26 +6,27 @@ class AuthTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-//        $options = array('file'=>__CLASS__.'.log');
-//        $this->_log = new \Devtools\Log($options);
+        $options = array('file'=>__CLASS__.'.log');
+        $this->_log = new \Devtools\Log($options);
     }
 
     public function tearDown()
     {
-//        $this->unlink(__CLASS__.'.log');
+        $this->unlink(__CLASS__.'.log');
     }
 
     public function testAuth()
     {
-//        $auth = new \Devtools\Auth("user", "password");
-//        $this->assertInstanceOf('Devtools\Auth', $auth);
-//        $this->_log->write('$auth is instance of Auth','EMPTY','authTest.log');
+        $auth = new \Devtools\Auth("user", "password");
+        $this->assertInstanceOf('Devtools\Auth', $auth);
+        $this->_log->write('$auth is instance of Auth','EMPTY','authTest.log');
     }
-
+/*
     public function testValidate()
     {
-//        $auth = new \Devtools\Auth("user", "password");
-//        $this->assertTrue($auth->validate("user", "password"));
-//        $this->assertFalse($auth->validate("user", "not password"));
+        $auth = new \Devtools\Auth("user", "password");
+        $this->assertTrue($auth->validate("user", "password"));
+        $this->assertFalse($auth->validate("user", "not password"));
     }
+*/
 }
