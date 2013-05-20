960c9f6737155280792673b6b30afb086418ed90
Jeremy Seago <seagoj@gmail.com>
2013-05-20 13:35:55 -0500

    Outputs $prefix

--- Markdown.php
+++ Markdown.php
@@ -82,6 +82,7 @@ class Markdown
                 // ORDERED LIST
                 if($pivot = strpos($line, '. ')!==false) {
                     $this->_log->write('Ordered List found');
+                    $this->_log->write(substr($line, 0, $pivot));
                     if(is_integer(trim($prefix = substr($line, 0, $pivot)))) {
                         $this->_log->write('Prefix is an integer');
                         $closeTag = 'ol';
