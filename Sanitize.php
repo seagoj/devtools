2586b7292165361a477941fe72df4cf8651f3797
Jeremy Seago <jseago@bellevuerx.com>
2014-02-26 22:26:52 -0600

    Adds Sanitize class for sanitization of inputs

---
+++ Sanitize.php
@@ -0,0 +1,18 @@
+<?php
+
+class Sanitize
+{
+    public static function str($dirty) {
+        return strip_tags(
+            htmlentities(
+                stripslashes($dirty)
+            )
+        );
+    }
+
+    public static function sql($dirty) {
+        return Sanitize::str(
+            mysql_real_escape_string($dirty)
+        );
+    }
+}
