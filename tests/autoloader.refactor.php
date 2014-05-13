15beca9af3f07c7496b9db199f46da609f9410c3
Jeremy Seago <jseago@bellevuerx.com>
2014-05-13 11:37:17 -0500

    test

--- tests/autoloader.refactor.php
+++ tests/autoloader.refactor.php
@@ -46,7 +46,7 @@ if ($cwd[1]!==$script_path[1]) {
     $libDir = array_pop($rootPathArray);
 } else {
     while ($cwd[0] === $script_path[0]) {
-            array_shift($cwd);
+            var_dump(array_shift($cwd));
             array_shift($script_path);
     }
     var_dump($cwd);
