3f8454323a7bb394b34f49732628776a438a31bf
Jeremy Seago <jseago@bellevuerx.com>
2014-05-13 10:00:44 -0500

    fixes autoloader

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -13,13 +13,13 @@ function findLibDir($pathArray)
 {
     $libDirArray = array('vendor', 'lib');
     $found = false;
-    
+
     // Traverse path to find path to libDir
-    while(!$found) {
+    while (!$found) {
         $path = implode("/", $pathArray);
         foreach ($libDirArray as $dir) {
-            if( is_dir("$path/$dir") ) {
-                array_push($pathArray, $dir); 
+            if ( is_dir("$path/$dir") ) {
+                array_push($pathArray, $dir);
                 $found = true;
                 break;
             }
@@ -31,42 +31,55 @@ function findLibDir($pathArray)
 
 // Set initial values
 $autoloadClass = "\Devtools\Autoload";
-$currentPathArray = explode("/", dirname(getcwd()."/".$_SERVER['SCRIPT_FILENAME']));
-$currentPathDepth = count($currentPathArray);
-if($currentPathArray[$currentPathDepth-1] === 'bin' &&
-    $currentPathArray[$currentPathDepth-2] === 'vendor' )
-{
-    array_pop($currentPathArray);
-    array_pop($currentPathArray);
-    $currentPathDepth -= 2;
-}
-$rootPathArray = findLibDir($currentPathArray);
-$libDir = array_pop($rootPathArray);
-$rootPathDepth = count($rootPathArray);
 
-// Ignore the parts of the path that are the same for each
-for ($i=0; $i<min($rootPathDepth, $currentPathDepth); $i++)
-{
-    if ($currentPathArray[$i] !== $rootPathArray[$i]) break;
-}
+$cwd = explode('/', getcwd());
+$script_path = explode('/', $_SERVER['SCRIPT_FILENAME']);
 
-// Build the relative path from currentDir to libDir
-$relPath = array();
-for ($j = $i; $j<$currentPathDepth; $j++) {
-    array_push($relPath, "..");
-}
-for ($i; $i<$rootPathDepth; $i++) {
-    array_push($relPath, array_pop($rootPathArray[$i]));
+if ($cwd[1]!==$script_path[1]) {
+    $relPath = $cwd;
+    $rootPathArray = findLibDir($cwd);
+    $libDir = array_pop($rootPathArray);
+} else {
+    $currentPathArray = explode("/", dirname(getcwd()."/".$_SERVER['SCRIPT_FILENAME']));
+
+    $currentPathDepth = count($currentPathArray);
+    if ($currentPathArray[$currentPathDepth-1] === 'bin'
+        && $currentPathArray[$currentPathDepth-2] === 'vendor'
+    ) {
+        array_pop($currentPathArray);
+        array_pop($currentPathArray);
+        $currentPathDepth -= 2;
+    }
+    $rootPathArray = findLibDir($currentPathArray);
+    $libDir = array_pop($rootPathArray);
+    $rootPathDepth = count($rootPathArray);
+
+    // Ignore the parts of the path that are the same for each
+    for ($i=0; $i<min($rootPathDepth, $currentPathDepth); $i++) {
+        if ($currentPathArray[$i] !== $rootPathArray[$i]) {
+            break;
+        }
+    }
+
+    // Build the relative path from currentDir to libDir
+    $relPath = array();
+    for ($j = $i; $j<$currentPathDepth; $j++) {
+        array_push($relPath, "..");
+    }
+    for ($i; $i<$rootPathDepth; $i++) {
+        array_push($relPath, array_pop($rootPathArray[$i]));
+    }
 }
 
 // Include file and register proper autoload function
-if(!empty($relPath)) {
+if (!empty($relPath)) {
     $relPath = implode("/", $relPath)."/";
 } else {
     $relPath = "";
 }
+
 $autoloadPath = $relPath.$libDir.str_replace('\\', '/', $autoloadClass).".php";
-include_once $autoloadPath;
+require_once $autoloadPath;
 $autoloadClass::register();
 
 if (isset($_REQUEST['debug'])) var_dump(spl_autoload_functions());
