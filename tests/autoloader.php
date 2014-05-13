49d6f97c0d7995412978bec6841faa1b97752d80
Jeremy Seago <jseago@bellevuerx.com>
2014-05-13 14:01:01 -0500

    use test autoloader.php

--- tests/autoloader.php
+++ tests/autoloader.php
@@ -45,40 +45,47 @@ if ($cwd[1]!==$script_path[1]) {
     $rootPathArray = findLibDir($cwd);
     $libDir = array_pop($rootPathArray);
 } else {
-    $currentPathArray = explode(
-        "/",
-        dirname(getcwd()."/".$_SERVER['SCRIPT_FILENAME'])
-    );
+    while ($cwd[0] === $script_path[0]) {
+            var_dump(array_shift($cwd));
+            array_shift($script_path);
+    }
+    var_dump($cwd);
+    var_dump($script_path);
 
-    $currentPathDepth = count($currentPathArray);
-    if ($currentPathArray[$currentPathDepth-1] === 'bin'
-        && $currentPathArray[$currentPathDepth-2] === 'vendor'
-    ) {
-        array_pop($currentPathArray);
-        array_pop($currentPathArray);
-        $currentPathDepth -= 2;
-    }
-    $rootPathArray = findLibDir($currentPathArray);
-    $libDir = array_pop($rootPathArray);
-    $rootPathDepth = count($rootPathArray);
+    /* $currentPathArray = explode( */
+    /*  "/", */
+    /*  dirname(getcwd()."/".$_SERVER['SCRIPT_FILENAME']) */
+    /* ); */
 
-    // Ignore the parts of the path that are the same for each
-    for ($i=0; $i<min($rootPathDepth, $currentPathDepth); $i++) {
-        if ($currentPathArray[$i] !== $rootPathArray[$i]) {
-            break;
-        }
-    }
+    /* $currentPathDepth = count($currentPathArray); */
+    /* if ($currentPathArray[$currentPathDepth-1] === 'bin' */
+    /*  && $currentPathArray[$currentPathDepth-2] === 'vendor' */
+    /* ) { */
+    /*  array_pop($currentPathArray); */
+    /*  array_pop($currentPathArray); */
+    /*  $currentPathDepth -= 2; */
+    /* } */
+    /* $rootPathArray = findLibDir($currentPathArray); */
+    /* $libDir = array_pop($rootPathArray); */
+    /* $rootPathDepth = count($rootPathArray); */
 
-    var_dump($currentPathArray);
-    var_dump($rootPathArray);
-    // Build the relative path from currentDir to libDir
-    $relPath = array();
-    for ($j = $i; $j<$currentPathDepth; $j++) {
-        array_push($relPath, "..");
-    }
-    for ($i; $i<$rootPathDepth; $i++) {
-        array_push($relPath, array_pop($rootPathArray[$i]));
-    }
+    /* // Ignore the parts of the path that are the same for each */
+    /* for ($i=0; $i<min($rootPathDepth, $currentPathDepth); $i++) { */
+    /*  if ($currentPathArray[$i] !== $rootPathArray[$i]) { */
+    /*      break; */
+    /*  } */
+    /* } */
+
+    /* var_dump($currentPathArray); */
+    /* var_dump($rootPathArray); */
+    /* // Build the relative path from currentDir to libDir */
+    /* $relPath = array(); */
+    /* for ($j = $i; $j<$currentPathDepth; $j++) { */
+    /*  array_push($relPath, ".."); */
+    /* } */
+    /* for ($i; $i<$rootPathDepth; $i++) { */
+    /*  array_push($relPath, array_pop($rootPathArray[$i])); */
+    /* } */
 }
 
 // Include file and register proper autoload function
