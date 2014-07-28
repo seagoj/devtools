269cae60cd8a5dbf294ccf63f604b496b7fc583d
Jeremy Seago <jseago@bellevuerx.com>
2014-07-28 13:11:51 -0500

    adds request::post

--- Response.php
+++ Response.php
@@ -102,6 +102,23 @@ class Response
         }
     }
 
+    public static function ajax($url='', $request=array())
+    {
+        global $debugLog;
+        if (!empty($url)) {
+            $options = array(
+                'http' => array(
+                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'method'  => 'POST',
+                    'content' => http_build_query($request),
+                ),
+            );
+            $context  = stream_context_create($options);
+            $result = file_get_contents($url, false, $context);
+            $debugLog->write($result);
+        }
+    }
+
     /**
      * load
      *
