4f5724fc8dcd66f5a373809110fb96324658e2cb
Jeremy Seago <jseago@bellevuerx.com>
2014-07-29 14:08:16 -0500

    logging

--- Model.php
+++ Model.php
@@ -417,9 +417,11 @@ class Model // implements IModel
      **/
     private function queryFirebird($sql, $reduce)
     {
+        global $debugLog;
         $sql = str_replace("\'", "''", $sql);
         if (gettype($this->connection) === 'resource') {
             $q = ibase_query($this->connection, $sql);
+            $debugLog->write($q);
             if (!(is_bool($q) || is_int($q))) {
                 $result = array();
                 while ($row = ibase_fetch_assoc($q, IBASE_TEXT)) {
@@ -429,6 +431,7 @@ class Model // implements IModel
             } else {
                 $result = $q;
             }
+            $debugLog->write($result);
             return ($reduce ? $this->reduceResult($result) : $result);
         } else {
             throw new \InvalidArgumentException('Invalid connection type.');
