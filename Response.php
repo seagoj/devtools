bae216610d591e6730818719a917fe862daed672
Jeremy Seago <seagoj@gmail.com>
2014-09-10 12:17:52 -0500

    tests resoponse

--- Response.php
+++ Response.php
@@ -420,6 +420,7 @@ class Response implements IService, \Serializable
     public function php($serialize = false)
     {
        $ret = array();
+       var_dump($this->__sleep());
        foreach ($this->__sleep() as $property) {
            $ret[$property] = $this->$property;
        }
