ddbd82a442b68bea72e8b37b340e9a5e443739a0
Jeremy Seago <seagoj@gmail.com>
2014-09-15 15:19:15 -0500

    Adds IoC

---
+++ IoC.php
@@ -0,0 +1,23 @@
+<?php namespace Devtools;
+
+abstract class IoC
+{
+    protected static $objectDefinitions = array();
+    protected static $registryOfCreatedObjects = array();
+
+    public static function bind($objectName, \Closure $callable)
+    {
+        self::$objectDefinitions[$objectName] = $callable;
+
+        return true;
+    }
+
+    public static function make($objectName)
+    {
+        if (isset(self::$registryOfCreatedObjects[$objectName])) {
+            return self::$registryOfCreatedObjects[$objectName];
+        }
+        $func = self::$objectDefinitions[$objectName];
+        return self::$registryOfCreatedObjects[$objectName] = $func();
+    }
+}
