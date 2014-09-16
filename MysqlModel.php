5d034aa5bbe1359b8b334e358750d9aa9d16f37e
Jeremy Seago <seagoj@gmail.com>
2014-09-16 17:39:54 -0500

    test

--- MysqlModel.php
+++ MysqlModel.php
@@ -165,7 +165,7 @@ class MysqlModel extends Model
      * @return array|string|integer Result of query
      * @author Jeremy Seago <seagoj@gmail.com>
      **/
-    public function query($queryString, $params=null, $reduce=false, $fetchType=\PDO::ATTR_DEFAULT_FETCH_MODE)
+    public function query($queryString, $params=null, $reduce=false, $fetchType=\PDO::FETCH_BOTH)
     {
         $stmt = $this->connection->prepare($queryString);
         if (!is_null($params)) {
