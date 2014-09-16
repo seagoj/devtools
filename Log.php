2a93d5dd7d849bd1c92749986101f5f162dceb8f
Jeremy Seago <seagoj@gmail.com>
2014-09-16 15:24:47 -0500

    test

--- Log.php
+++ Log.php
@@ -262,7 +262,7 @@ class Log
             if (isset($frame['args'])) {
                 $args = array();
                 foreach ($frame['args'] as $arg) {
-                    if (is_string($arg) || get_class($arg) === 'PDOStatement') {
+                    if (is_string($arg) || ((is_object($arg) || is_array($arg)) && get_class($arg) === 'PDOStatement')) {
                         $args[] = "'" . $arg . "'";
                     } elseif (is_array($arg)) {
                         $args[] = serialize($arg);
