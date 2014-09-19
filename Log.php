7fa8b688f150e6b14b4bfee0eed1ce088b0f68ce
Jeremy Seago <seagoj@gmail.com>
2014-09-19 12:03:06 -0500

    test

--- Log.php
+++ Log.php
@@ -270,6 +270,7 @@ class Log
                         $args[] = ($arg) ? "true" : "false";
                     } elseif (is_object($arg) || is_array($arg)) {
                         try {
+                            var_dump($arg);
                             $args[] = serialize($arg);
                         } catch (\PDOException $e) {
                             $args[] = $e->getMessage();
