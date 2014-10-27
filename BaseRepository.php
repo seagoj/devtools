987a3f53eb8858348d8121c0afef3f0b3e58ef75
Jeremy Seago <seagoj@gmail.com>
2014-10-27 12:28:51 -0500

    test

--- BaseRepository.php
+++ BaseRepository.php
@@ -48,7 +48,7 @@ abstract class BaseRepository
         }
 
         $this->apply(
-            $this->all()->where($filter)
+            $this->all()->where($filter)->get()
         );
 
         return $this;
