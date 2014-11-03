ef4fa2ddb1a0d0d2d913462db1d4751df2dfd36f
Jeremy Seago <seagoj@gmail.com>
2014-11-03 17:02:09 -0600

    test

--- PDORepository.php
+++ PDORepository.php
@@ -83,11 +83,6 @@ abstract class PDORepository extends BaseRepository implements Repository
             : $stmt->execute();
         $data = $stmt->fetchAll($fetchType);
 
-        var_dump($query);
-        var_dump($params);
-        var_dump($data);
-        var_dump($stmt->errorInfo());
-
         $this->prepareResponseData($data, $executionResult);
 
         return $reduce
