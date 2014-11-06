274a6460f91cbc7a1c6a61bd3604358c3da013f4
Jeremy Seago <seagoj@gmail.com>
2014-11-06 15:42:01 -0600

    Adds BaseEntity

---
+++ BaseEntity.php
@@ -0,0 +1,41 @@
+<?php namespace Devtools;
+
+abstract class BaseEntity
+{
+    protected $repository;
+    protected $log;
+
+    public function __construct(
+        Devtools\Repository $repository,
+        Devtools\Log $log
+    ) {
+        $this->repository = $repository;
+        $this->log = $log;
+    }
+
+    public function __get($property)
+    {
+        return $this->repository->$property
+            ? $this->repository->$property
+            : null;
+    }
+
+    public function __clone()
+    {
+        $this->repository = clone $this->repository;
+    }
+
+    public function byId($id)
+    {
+        $this->repository->find($id)->get();
+        return clone $this;
+    }
+
+    public function byName(Array $namePair)
+    {
+        $this->repository->findBy(
+            $namePair
+        )->get();
+        return clone $this;
+    }
+}
