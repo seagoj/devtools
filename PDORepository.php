a843f296995c71ddae7aa89ff205eae84492baa0
Jeremy Seago <seagoj@gmail.com>
2014-11-11 10:09:40 -0600

    test

--- PDORepository.php
+++ PDORepository.php
@@ -264,12 +264,7 @@ abstract class PDORepository extends BaseRepository implements Repository
             } catch (\Exception $e) {
                 $this->log->write($e->getCode());
                 $this->log->write($e->getMessage());
-                if ($e->getCode() == 0) {
-                    $lastInsertId = 0;
-                } else {
-                    $this->log->write($e->getCode());
-                    /* throw $e; */
-                }
+                $lastInsertId = 0;
             }
 
             $isInsertStatement = $executionResult
