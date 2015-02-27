a86aa39375d094c9aaa353115ea9e01d519759ba
Jeremy Seago <seagoj@gmail.com>
2015-02-26 22:10:18 -0600

    test

--- RandData.php
+++ RandData.php
@@ -1,57 +1,9 @@
 <?php namespace Devtools;
-/**
- * Random data generator
- *
- * @category    Seagoj
- * @package     Devtools
- * @author      Jeremy Seago <seagoj@gmail.com>
- * @license     http://github.com/seagoj/Devtools/LICENSE MIT
- * @link        http://github.com/seagoj/Devtools
- **/
 
-/**
- * Class RandData
- *
- * @category    Seagoj
- * @package     Devtools
- * @author      Jeremy Seago <seagoj@gmail.com>
- * @license     http://github.com/seagoj/Devtools/LICENSE MIT
- * @link        http://github.com/seagoj/Devtools
- *
- * Returns random values of the passed type
- **/
 class RandData
 {
-    /**
-     * Array of valid data types
-     *
-     * Passed types are validataed against values in this array.
-     **/
     private $dataTypes;
 
-    /**
-     * RandData::__construct
-     *
-     * Constructor for RandData class
-     *
-     * Populates valid data types into this.dataTypes
-     **/
-    public function __construct()
-    {
-        $this->dataTypes = array('String','Array','Integer','Bool','Double');
-    }
-
-    /**
-     * RandData::get
-     *
-     * Returns random data
-     *
-     * Returns random data of type $type
-     *
-     * @param string $type Type of random data to be returned
-     *
-     * @returns     $type   Random data
-     **/
     public function get($type)
     {
         $func = 'rand'.ucfirst(strtolower($type));
@@ -63,16 +15,7 @@ class RandData
         }
     }
 
-    /**
-     * RandData::randArray
-     *
-     * Returns array of max size $max with random values
-     *
-     * @param integer $max max size of array
-     *
-     * @return array array of random values
-     **/
-    private function randArray($max = 100)
+    public static function randArray($max = 100)
     {
         $array = array();
         $arrayLen = rand()%$max;
@@ -84,30 +27,12 @@ class RandData
         return $array;
     }
 
-    /**
-     * RandData::randInteger
-     *
-     * Returns random integer of max size $max
-     *
-     * @param integer $max max size of integer; defaults to PHP_INT_MAX
-     *
-     * @return integer integer of max size $max
-     **/
-    private function randInteger($max = PHP_INT_MAX)
+    public static function randInteger($max = PHP_INT_MAX)
     {
         return randData::randSign()*rand()%$max;
     }
 
-    /**
-     * RandData::randDouble
-     *
-     * Returns random double of max size $max
-     *
-     * @param integer $max max size of double; defaults to random
-     *
-     * @return double double of max size $max
-     **/
-    private function randDouble($max = 0)
+    public static function randDouble($max = 0)
     {
         if ($max == 0) {
             $max = mt_getrandmax();
@@ -116,41 +41,17 @@ class RandData
         return randData::randSign()*mt_rand() / $max * mt_rand();
     }
 
-    /**
-     * RandData::randSign
-     *
-     * Returns random sign based on random generated number
-     *
-     * @return int Returns either a 1 or -1 depending on random outcome
-     **/
-    private function randSign()
+    public static function randSign()
     {
         return pow(-1, rand(0, 1));
     }
 
-    /**
-     * RandData::randBool
-     *
-     * Returns random boolean based on random generated number
-     *
-     * @return boolean Returns true or false depending on random outcome
-     **/
-    private function randBool()
+    public static function randBool()
     {
         return (bool) rand(0, 1);
     }
 
-    /**
-     * RandData::randString
-     *
-     * Returns random string of max length $max
-     *
-     * @param integer $max Maximum length of string to return; defaults
-     *                              to 100
-     *
-     * @return string Random string of max length $max
-     **/
-    private function randString($max = 100)
+    public static function randString($max = 100)
     {
         $stringLen = rand()%$max;
         $string = "";
