0ec03beaa0272281be7bd7549e5e62452f89b5fe
Jeremy Seago <seagoj@gmail.com>
2015-04-19 18:36:46 -0500

    adds constructor

--- Query.php
+++ Query.php
@@ -4,4 +4,10 @@ class Query
 {
     public $sql;
     public $params;
+
+    public function __construct($sql, $params = null)
+    {
+        $this->sql = $sql;
+        $this->params = $params;
+    }
 }
