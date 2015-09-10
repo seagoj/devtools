12c2bd6fc909cec692042d2d86df6fc9bef2f099
Jeremy Seago <seagoj@gmail.com>
2015-09-10 15:05:49 -0500

    adds ability to perform basic math on currencies

--- Currency/Currency.php
+++ Currency/Currency.php
@@ -1,12 +1,12 @@
 <?php namespace Devtools\Currency;
 
-abstract class Currency
+class Currency
 {
     protected $sigfig;
     protected $symbol;
     protected $separation;
 
-    public function __construct($value)
+    public function __construct($value = 0)
     {
         $this->value = $this->format($value);
     }
@@ -19,6 +19,56 @@ abstract class Currency
     public function __toString()
     {
         return "{$this->symbol}{$this->value}";
+    }
+
+    public static function add(Currency $currency1, Currency $currency2)
+    {
+        $currencyType = self::type($currency1, $currency2);
+
+        return new $currencyType(
+            $currency1->value() + $currency2->value()
+        );
+    }
+
+    public static function subtract(Currency $currency1, Currency $currency2)
+    {
+        $currencyType = self::type($currency1, $currency2);
+
+        return new $currencyType(
+            $currency1->value() - $currency2->value()
+        );
+    }
+
+    public static function multiply(Currency $currency, $multiple)
+    {
+        $currencyType = get_class($currency);
+
+        return new $currencyType(
+            $currency->value * $multiple
+        );
+    }
+
+    public static function divide(Currency $currency, $divisor)
+    {
+        $currentType = get_class($currency);
+
+        return new $currentType(
+            $currency->value / $divisor
+        );
+    }
+
+    private static function type(Currency $currency1, Currency $currency2)
+    {
+        self::ensureTypeEquality($currency1, $currency2);
+
+        return get_class($currency1);
+    }
+
+    private static function ensureTypeEquality(Currency $currency1, Currency $currency2)
+    {
+        if (get_class($currency1) !== get_class($currency2)) {
+            throw new Exception('Cannot add two different denominations.');
+        }
     }
 
     private function format($value)
--- spec/Devtools/Currency/DollarSpec.php
+++ spec/Devtools/Currency/DollarSpec.php
@@ -2,6 +2,7 @@
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Devtools\Currency\Dollar;
 
 class DollarSpec extends ObjectBehavior
 {
@@ -23,5 +24,34 @@ class DollarSpec extends ObjectBehavior
     function it_prints_formatted_value_as_string()
     {
         $this->__toString()->shouldReturn('$12.00');
+    }
+
+    function it_should_add_two_currencies()
+    {
+        $c2 = new Dollar(15);
+        $this::add($this, $c2)->shouldHaveType('Devtools\Currency\Dollar');
+        $this::add($this, $c2)->value()->shouldReturn(27.00);
+    }
+
+    function it_should_subtract_two_currencies()
+    {
+        $c2 = new Dollar(10);
+        $this::subtract($this, $c2)->shouldHaveType('Devtools\Currency\Dollar');
+        $this::subtract($this, $c2)->value()->shouldReturn(2.00);
+
+        $this::subtract($c2, $this)->shouldHaveType('Devtools\Currency\Dollar');
+        $this::subtract($c2, $this)->value()->shouldReturn(-2.00);
+    }
+
+    function it_should_multiply_a_currency()
+    {
+        $this::multiply($this, 3)->shouldHaveType('Devtools\Currency\Dollar');
+        $this::multiply($this, 3)->value()->shouldReturn(36.00);
+    }
+
+    function it_should_divide_a_currency()
+    {
+        $this::divide($this, 2)->shouldHaveType('Devtools\Currency\Dollar');
+        $this::divide($this, 2)->value()->shouldReturn(6.00);
     }
 }
